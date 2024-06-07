6be533da791780bd08ffafa4e5e197dd66ce328d
hanmaul <arramsy0902@gmail.com>
2024-06-07 09:04:32 +0700

    update

--- lib/data/repository/webview_repository.dart
+++ lib/data/repository/webview_repository.dart
@@ -4,7 +4,7 @@ class WebviewRepository {
   Future<String> getUrlAkunku() async {
     String key = await DatabaseRepository().loadUser(field: "key");
     String url =
-        "http://mobilekamm.ddns.net:8065/m_mlp/mobile/akun/profile?key=";
+        "https://www.kamm-group.com:8070/m_mlp/mobile/akun/profile?key=";
     String urlAkunku = url + key;
     return urlAkunku;
   }
@@ -12,14 +12,14 @@ class WebviewRepository {
   Future<String> getUrlRegister() async {
     String key = await DatabaseRepository().loadUser(field: "key");
     String url =
-        "http://mobilekamm.ddns.net:8065/m_mlp/mobile/akun/registrasi/data-diri?key=";
+        "https://www.kamm-group.com:8070/m_mlp/mobile/akun/registrasi/data-diri?key=";
     String urlRegister = url + key;
     return urlRegister;
   }
 
   Future<String> getUrlHistory() async {
     String custId = await DatabaseRepository().loadUser(field: "custId");
-    String url = "http://mobilekamm.ddns.net:8065/mls/mobile/kp/";
+    String url = "https://www.kamm-group.com:8070/mls/kp/";
     String urlHistory = url + custId;
     return urlHistory;
   }
