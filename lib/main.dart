b37c818e7f10f8e4946c53c2430748e103d3cf01
hanmaul <arramsy0902@gmail.com>
2024-10-21 10:52:08 +0700

    - flag to auto update home page at first launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loyalty/bloc/auth/auth_bloc.dart';
 import 'package:loyalty/data/repository/database_repository.dart';
